391b73f35b1660ac05e2dbff23c85cad7e8d1627
Gene Gotimer <otherdevopsgene@portinfo.com>
2026-04-05 14:52:40 -0400

    fix: updated for 2026

--- extras/terraform-example/main.tf
+++ extras/terraform-example/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.49.0"
+      version = "~> 6.39.0"
     }
   }
 
-  required_version = ">= 1.8.3"
+  required_version = ">= 1.14.0"
 }
 
 provider "aws" {
@@ -15,8 +15,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_server" {
-  ami           = "ami-0629230e074c580f2"
-  instance_type = "t2.micro"
+  ami           = "ami-0823767a63030e222"
+  instance_type = "t4g.nano"
 
   tags = {
     Name = "my-server"
